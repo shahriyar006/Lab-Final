18e5f92751823c14fbfb06c37659369082243b17
S.M shahriyar Engineer <68679455+shahriyar006@users.noreply.github.com>
2022-04-18 22:56:40 +0600

    Add files via upload

---
+++ riyaz/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'riyaz';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
